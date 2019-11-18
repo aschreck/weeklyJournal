70d6e1d07b75e2f2137a860c99b6b232a2a60aa6
Aaron Schreck <schreck.aaron@gmail.com>
2019-11-17 18:57:07 -0700

    migrate initial WIP

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WelcomeButton from './WelcomeButton'
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Ready to Journal?
+      </h1>
+      <WelcomeButton />
+    </div>
+  );
+}
+
+export default App;
