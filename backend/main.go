0cab96b5bc4649ca6b064571d84f7a6e40644894
Aaron Schreck <schreck.aaron@gmail.com>
2019-11-24 14:26:52 -0700

    WIP add function to check for or create existing journal entry

--- backend/main.go
+++ backend/main.go
@@ -5,22 +5,82 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 // Entry asdfas
-type Entry struct {
+type EntryElement struct {
 	Date     string `json:"date"`
 	Category string `json:"category"`
 	Content  string `json:"content"`
 }
 
+type Entry struct {
+	Categories struct {
+		Review struct {
+			Content string `json:"content"`
+		} `json:"review"`
+		Gratitude struct {
+			Content string `json:"content"`
+		} `json:"gratitude"`
+		Goals struct {
+			Content string `json:"content"`
+		} `json:"goals"`
+		Projects struct {
+			Content string `json:"content"`
+		} `json:"projects"`
+		Thoughts struct {
+			Contents string `json:"contents"`
+		} `json:"thoughts"`
+	} `json:"categories"`
+}
+
 var apiPath = "/api/v1"
 
 func main() {
-	apiPath := "/api/v1"
-
+	http.HandleFunc(apiPath+"/doesEntryExist", doesEntryExist)
 	http.HandleFunc(apiPath+"/journalEntry", journalPost)
+	http.HandleFunc(apiPath+"/readEntry", readEntry)
+
 	http.ListenAndServe(":8080", nil)
+}
+
+func readEntry(w http.ResponseWriter, r *http.Request) {
+	// this function needs to do one of two things:
+	// 1) if a query parameter is provided, it needs to send back only the section of JSON associated with that category
+	// 2) If there is no query parameter specified, return the whole file.
+
+	// Brainstorm: one solution here might be to automatically create each new entry with all of the relevant fields and just set them to empty strings. that would homogenize my JSON format and make sure I don't need to deal with the problem of things not existing.
+	if r.URL.Path != apiPath+"/readEntry" {
+		http.Error(w, "404 not found", 404)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+	}
+
+	// returns a map of all the query parameters.
+	parameters := r.URL.Query()
+
+	// not ok if the key I'm reading from does not exist.
+	dateArr, ok := parameters["date"]
+	date := dateArr[0]
+	if !ok {
+		http.Error(w, "Malformed — must provide journal date", 400)
+	}
+
+	// TODO: regex validate the date format.
+
+	// try to read a file with this date.
+	data, err := ioutil.ReadFile("entries/" + date + ".json")
+	jsonString := string(data)
+	if err != nil {
+		http.Error(w, "File not found", 404)
+	}
+
+	fmt.Println(jsonString)
 }
 
 func journalPost(w http.ResponseWriter, r *http.Request) {
@@ -34,7 +94,8 @@ func journalPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var e Entry
+	// fmt.Println(r.URL.Query())
+	var e EntryElement
 	// assume that the incoming request has a JSON body
 	err := json.NewDecoder(r.Body).Decode(&e)
 
@@ -42,6 +103,8 @@ func journalPost(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+
+	// check for an existing file with this date. We're going to load the file into memory and edit it as such
 
 	// start by writing the entries to a file.
 	fmt.Fprintf(w, "Entry: %+v", e)
@@ -56,4 +119,95 @@ func journalPost(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func startNewWeek(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func doesEntryExist(w http.ResponseWriter, r *http.Request) {
+	//  This function is simply going to check whether we have an entry for this week.
+	date := time.Now()
+	weekday := date.Weekday().String()
+	day := date.Day()
+	monthName := date.Month().String()
+	year := strconv.Itoa(date.Year())
+	outputDay, outputMonth := ComputePreviousSaturdayDate(weekday, day, monthName, year)
+
+	fmt.Println(outputDay, outputMonth)
+	// if I know the day of the week and the date, I can
+	// subtract the date from the number above to find what the date of the Saturday was.
+}
+
+func ComputePreviousSaturdayDate(weekday string, day int, monthName string, year string) (int, string) {
+	dayWeekDiffValues := map[string]int{
+		"Saturday":  0,
+		"Sunday":    1,
+		"Monday":    2,
+		"Tuesday":   3,
+		"Wednesday": 4,
+		"Thursday":  5,
+		"Friday":    6,
+	}
+
+	daysInMonth := map[string]int{
+		"January":   31,
+		"February":  28,
+		"March":     31,
+		"April":     30,
+		"May":       31,
+		"June":      30,
+		"July":      31,
+		"August":    31,
+		"September": 30,
+		"October":   31,
+		"November":  30,
+		"December":  31,
+	}
+
+	numberToMonth := map[int]string{
+		1:  "January",
+		2:  "February",
+		3:  "March",
+		4:  "April",
+		5:  "May",
+		6:  "June",
+		7:  "July",
+		8:  "August",
+		9:  "September",
+		10: "October",
+		11: "November",
+		12: "December",
+	}
+
+	monthToNumber := map[string]int{
+		"January":   1,
+		"February":  2,
+		"March":     3,
+		"April":     4,
+		"May":       5,
+		"June":      6,
+		"July":      7,
+		"August":    8,
+		"September": 9,
+		"October":   10,
+		"November":  11,
+		"December":  12,
+	}
+
+	fmt.Println(weekday, monthName, year)
+	saturdayDateDiff := day - dayWeekDiffValues[weekday]
+
+	var outputDay int
+	var outputMonth string
+	if saturdayDateDiff < 0 {
+		outputMonth := numberToMonth[monthToNumber[monthName]-1]
+		outputMonthDays := daysInMonth[outputMonth]
+		outputDay = outputMonthDays - saturdayDateDiff
+	} else {
+		outputDay = day - saturdayDateDiff
+		outputMonth = monthName
+	}
+
+	return outputDay, outputMonth
 }
---
+++ backend/test/main_test.go
@@ -0,0 +1,15 @@
+package main_test
+
+import "testing"
+
+func TestComputePreviousSaturdayDate(t *testing.T) {
+	outputDay, outputMonth := ComputePreviousSaturdayDate("Tuesday", 26, "November", 2019)
+
+	if outputDay != 24 {
+		t.Errorf("Day was incorrect; got %d, want: %d", outputMonth, 23)
+	}
+
+	if outputMonth != "November" {
+		t.Errorf("Day was incorrect; got %s, want: %s", outputMonth, "November")
+	}
+}
